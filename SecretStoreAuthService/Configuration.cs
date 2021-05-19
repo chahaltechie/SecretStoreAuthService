d41eebe7b047d455dc321b5b9d78ade4aa54aa37
Ravinder Chahal <chahaltechie@gmail.com>
2021-05-19 10:42:00 +0530

    FIrst commit

---
+++ SecretStoreAuthService/Configuration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SecretStoreAuthService
+{
+    public class Configuration
+    {
+        public static AAD GetAadConfiguration()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            
+            AAD aadConfig = new();
+            configuration.Bind(nameof(AAD),aadConfig);
+            
+            aadConfig.Authority = string.Format(CultureInfo.InvariantCulture, 
+                aadConfig.Instance, aadConfig.TenantId);
+            return aadConfig;
+        }
+    }
+
+    public class AAD   
+    {
+        public string Instance {get; set;} 
+        public string TenantId {get; set;}
+        public string ClientId {get; set;}
+        public string Authority { get; set; }
+            
+
+        public string ClientSecret {get; set;}
+        public string BaseAddress {get; set;}
+        public string ResourceID {get; set;}
+        
+        
+    }
+}
---
+++ SecretStoreAuthService/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace SecretStoreAuthService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making the call...");
+            RunAsync().GetAwaiter().GetResult();
+        }
+        private static async Task RunAsync()
+        {
+            var config = Configuration.GetAadConfiguration();
+
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            string[] ResourceIds = new string[] {config.ResourceID};
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
